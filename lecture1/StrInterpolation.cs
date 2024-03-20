c77b296383d86f1b558ee68bcb65556812716b15
jaskuen <100339401+jaskuen@users.noreply.github.com>
2024-03-20 21:35:08 +0300

    First commit
    Adding tasks done on the lecture

---
+++ lecture1/StrInterpolation.cs
@@ -0,0 +1,12 @@
+﻿public class StrInterpolate
+{
+    public static void Main(string[] args)
+    {
+        string name = "Victor";
+        string surname = "Borge";
+
+        string fullname = $"{name} {surname}";
+
+        Console.WriteLine(fullname);
+    }
+}
---
+++ lecture1/test/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите название товара: ");
+string productName = Console.ReadLine();
+Console.Write("Введите кол-во единиц товара: ");
+string productCountStr = Console.ReadLine();
+int productCount = Int32.Parse(productCountStr);
+Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Введите адрес желаемой доставки: ");
+string deliveryAddress = Console.ReadLine();
+Console.WriteLine("Здравствуйте, " + username + ", вы заказали " + productCount + " " + productName + " на адрес " + deliveryAddress + ", все верно? Нажмите ENTER, чтобы подтвердить.");
+ConsoleKeyInfo key = Console.ReadKey();
+DateTime dateToday = DateTime.Now;
+if (key.Key == ConsoleKey.Enter)
+{
+    Console.WriteLine(username + "!");
+    Console.WriteLine("Ваш заказ " + productName + " в количестве " + productCount + " оформлен!");
+    Console.WriteLine("Ожидайте доставку по адресу " + deliveryAddress + " к " + dateToday.AddDays(3));
+}
+else
+{
+    Console.WriteLine(username + ", до следующего заказа!");
+}
+
