1143e009f8f5ad5a9517bbcaa1990572ad5bdf65
jaskuen <100339401+jaskuen@users.noreply.github.com>
2024-06-25 20:31:36 +0300

    Issue fixes
    Fixed some key names, value types and queries

--- homework 4/Query.sql
+++ homework 4/Query.sql
@@ -3,7 +3,7 @@
 		id_room INT IDENTITY(1, 1) NOT NULL,
 		room_number INT NOT NULL,
 		room_type NVARCHAR(30) NOT NULL,
-		price_per_night INT NOT NULL,
+		price_per_night DECIMAL(6, 2) NOT NULL,
 		is_available BIT NOT NULL, /*как я понял, есть два значения: 0 (false) и 1 (true).*/
 		
 		CONSTRAINT PK_rooms_id_room PRIMARY KEY (id_room),
@@ -47,7 +47,7 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='rooms_to_facilities')
 		id_facility INT NOT NULL,
 		id_room INT NOT NULL,
 
-		CONSTRAINT PK_rooms_to_facilities_id_room_facility PRIMARY KEY (id_facility, id_room),
+		CONSTRAINT PK_rooms_to_facilities_id_room_id_facility PRIMARY KEY (id_facility, id_room),
 
 		CONSTRAINT FK_rooms_id_room FOREIGN KEY (id_room) REFERENCES dbo.rooms (id_room),
 		CONSTRAINT FK_facilities_id_facility FOREIGN KEY (id_facility) REFERENCES dbo.facilities (id_facility)
@@ -63,9 +63,12 @@ VALUES
 
 INSERT INTO dbo.customers (first_name,	last_name, phone_number)
 VALUES
-	(N'Игорь', N'Пономарев', '+7 (999) 123-45-67'),
 	(N'Ольга', N'Мамаева', '+7 (111) 000-98-89'),
 	(N'Софья', N'Чичикова', '+7 (800) 555-35-36')
+
+INSERT INTO dbo.customers (first_name, last_name, phone_number, email)
+VALUES
+	(N'Игорь', N'Пономарев', '+7 (999) 123-45-67', 'igorponomarev@mail.ru')
 
 
 INSERT INTO dbo.bookings (id_room, id_customer, check_in_date, check_out_date)
@@ -88,14 +91,23 @@ VALUES
 	(5, 5), (5, 6), (5, 8)
 
 SELECT * FROM dbo.rooms WHERE (dbo.rooms.is_available = 1)
-SELECT * FROM dbo.customers WHERE(dbo.customers.last_name LIKE N'М%')
-SELECT * FROM dbo.bookings WHERE (dbo.bookings.id_customer = 8)
-SELECT * FROM dbo.bookings WHERE (dbo.bookings.id_room = 1)
-SELECT room_number FROM dbo.rooms 
-JOIN dbo.bookings ON (
-		'2024-04-17T10:00:00' NOT BETWEEN dbo.bookings.check_in_date AND dbo.bookings.check_out_date 
-		AND (
-		(dbo.bookings.id_room = dbo.rooms.id_room) AND (dbo.rooms.is_available = 0) OR
-		(dbo.bookings.id_room != dbo.rooms.id_room) AND (dbo.rooms.is_available = 1))
-	)
- 
+SELECT * FROM dbo.customers WHERE(dbo.customers.last_name LIKE N'S%')
+
+SELECT * FROM dbo.bookings AS booking
+	JOIN dbo.customers AS customer ON booking.id_customer = customer.id_customer
+	JOIN dbo.rooms AS room ON booking.id_room = room.id_room
+		WHERE customer.first_name = N'Игорь' AND customer.last_name = N'Пономарев' 
+			OR customer.email = 'igorponomarev@mail.ru';
+
+SELECT * FROM dbo.bookings AS booking
+	JOIN dbo.rooms AS room ON room.id_room = booking.id_room
+		WHERE room.room_number = 21
+
+SELECT * FROM dbo.rooms AS room
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM dbo.bookings AS booking
+    WHERE booking.id_room = room.id_room 
+		AND room.is_available = 1
+		AND '2024-04-11T10:00:00' BETWEEN booking.check_in_date AND booking.check_out_date
+)
